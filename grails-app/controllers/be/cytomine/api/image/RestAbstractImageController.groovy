f2dc30c70d0a1f6541db5ba7573d849f14e2040d
TheGeekTortoise <renaud.hoyoux@yahoo.fr>
2018-01-15 17:28:01 +0100

    reduce url number for merging channel
    (cherry picked from commit 56fe233)

--- grails-app/controllers/be/cytomine/api/image/RestAbstractImageController.groovy
+++ grails-app/controllers/be/cytomine/api/image/RestAbstractImageController.groovy
@@ -323,6 +323,9 @@ class RestAbstractImageController extends RestController {
 
             def images = imageSequenceService.list(group)
 
+            if(merge.equals("channel")){
+                images = images.findAll{it.zStack == sequence.zStack && it.time == sequence.time}
+            }
             log.info "all image for this group=$images"
 
 
@@ -358,8 +361,8 @@ class RestAbstractImageController extends RestController {
 
             def urls = []
 
-            (0..5).each {
-                urls << servers.get(myRandomizer.nextInt(servers.size())).url +"/"+ url
+            servers.each {
+                urls << it.url +"/"+ url
             }
 
             //retrieve all image instance (same sequence)
