b413e20bce7349c61ef0136c141aa8d26c7106fb
TheGeekTortoise <renaud.hoyoux@yahoo.fr>
2018-01-26 15:20:09 +0100

    fix deletion of images
    (cherry picked from commit 0cd49d9)

--- grails-app/services/be/cytomine/image/multidim/ImageSequenceService.groovy
+++ grails-app/services/be/cytomine/image/multidim/ImageSequenceService.groovy
@@ -181,6 +181,8 @@ class ImageSequenceService extends ModelService {
     def abstractImageService
     def deleteDependentImageInstance(ImageSequence domain, Transaction transaction, Task task = null) {
         imageInstanceService.delete(domain.image,transaction,null,false)
+        abstractImageService.delete(domain.image.baseImage)
+        abstractImageService.deleteFile(domain.image.baseImage)
     }
 
     def getStringParamsI18n(def domain) {
