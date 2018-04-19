10d3303145fa0478aaea9ff149d3d9329b08c5f2
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-04-19 10:42:15 +0200

    Removed from tracking

--- web-app/application/views/explorer/v2.0/src/helpers/humanDate.js
+++
@@ -1,6 +0,0 @@
-export default function (date) {
-    date = new Date(parseInt(date)).toISOString();
-    date = date.replace('T', ' ');
-    date = date.substring(0, date.indexOf('.'));
-    return date;
-}
