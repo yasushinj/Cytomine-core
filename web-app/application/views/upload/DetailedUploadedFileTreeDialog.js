f8ff9b0f62fc9a5edb410af97b33cd0223ccddbd
TheGeekTortoise <Renaud.Hoyoux@yahoo.fr>
2018-05-22 18:41:51 +0200

    add delete button

--- web-app/application/views/upload/DetailedUploadedFileTreeDialog.js
+++ web-app/application/views/upload/DetailedUploadedFileTreeDialog.js
@@ -29,6 +29,7 @@ var DetailedUploadedFileTreeDialog = Backbone.View.extend({
             });
         return this;
     },
+    tree:null,
 
     doLayout: function (tpl) {
         var self = this;
@@ -39,44 +40,7 @@ var DetailedUploadedFileTreeDialog = Backbone.View.extend({
 
         $(this.el).append(htmlCode);
 
-        $.get( new UploadedFileCollection({ root: self.model.id}).url(), function( data ) {
-            var nodes = [];
-            var allNodes = [];
-            var infos = self.getNodeInfo(data.collection[0]);
-            var node = {
-                title : infos.title,
-                expand : true,
-                key : data.collection[0].id,
-                noLink : true,
-                unselectable : true//,
-            };
-            nodes.push(node);
-            allNodes.push(node);
-
-            for(var i = 1; i<data.collection.length;i++) {
-
-                var parent = $.grep(allNodes, function( n ) {
-                    return ( n.key ==  data.collection[i].parentId );
-                })[0];
-
-                infos = self.getNodeInfo(data.collection[i]);
-                node = {
-                    title : infos.title,
-                    expand : true,
-                    key : data.collection[i].id,
-                    noLink : true,
-                    unselectable : true//,
-                };
-                if(parent.children == null) parent.children = [];
-                parent.children.push(node);
-                allNodes.push(node);
-            }
-
-            $("#detailedUploadedFileTreeDialog-"+self.model.id).find("#treefile").empty();
-            $("#detailedUploadedFileTreeDialog-"+self.model.id).find("#treefile").dynatree({
-                children: nodes
-            });
-        });
+        self.update();
 
         $("#detailedUploadedFileTreeDialog-"+self.model.id).on('click', '.previewUploadedFile', function(e) {
             var url = $(this).data('url');
@@ -102,7 +66,7 @@ var DetailedUploadedFileTreeDialog = Backbone.View.extend({
             });
         });
 
-        /*$("#detailedUploadedFileTreeDialog-"+self.model.id).on('click', ".deleteUploadedFile", function (e) {
+        $("#detailedUploadedFileTreeDialog-"+self.model.id).on('click', ".deleteUploadedFile", function (e) {
             var idUpload = $(e.currentTarget).data("id");
             var idImage = $(e.currentTarget).data("aiid");
 
@@ -110,9 +74,11 @@ var DetailedUploadedFileTreeDialog = Backbone.View.extend({
 
             DialogModal.initDialogModal(null, idUpload, 'UploadFile', 'Do you want to delete this image ?', 'CONFIRMATIONWARNING', function(){
                 var deleteUploadFile = function() {
+                    $("#detailedUploadedFileTreeDialog-"+self.model.id).modal('show');
                     new UploadedFileModel({id: idUpload}).destroy({
                         success: function (model, response) {
                             window.app.view.message("Uploaded file", "deleted", "success");
+                            self.update();
                         },
                         error: function (model, response) {
                             var json = $.parseJSON(response.responseText);
@@ -137,9 +103,57 @@ var DetailedUploadedFileTreeDialog = Backbone.View.extend({
             }, function(){
                 $("#detailedUploadedFileTreeDialog-"+self.model.id).modal('show');
             });
-        });*/
+        });
 
         $("#detailedUploadedFileTreeDialog-"+self.model.id).modal('show');
+    },
+
+    update : function () {
+        var self = this;
+
+        if(self.tree != null) {
+            $("#detailedUploadedFileTreeDialog-" + self.model.id).find("#treefile").dynatree("destroy");
+        }
+
+        $.get( new UploadedFileCollection({ root: self.model.id}).url(), function( data ) {
+
+            var nodes = [];
+            var allNodes = [];
+            var infos = self.getNodeInfo(data.collection[0]);
+            var node = {
+                title : infos.title,
+                expand : true,
+                key : data.collection[0].id,
+                noLink : true,
+                unselectable : true//,
+            };
+            nodes.push(node);
+            allNodes.push(node);
+
+
+            for(var i = 1; i<data.collection.length;i++) {
+
+                var parent = $.grep(allNodes, function( n ) {
+                    return ( n.key ==  data.collection[i].parentId );
+                })[0];
+
+                infos = self.getNodeInfo(data.collection[i]);
+                node = {
+                    title : infos.title,
+                    expand : true,
+                    key : data.collection[i].id,
+                    noLink : true,
+                    unselectable : true//,
+                };
+                if(parent.children == null) parent.children = [];
+                parent.children.push(node);
+                allNodes.push(node);
+            }
+
+            self.tree = $("#detailedUploadedFileTreeDialog-"+self.model.id).find("#treefile").dynatree({
+                children: nodes
+            });
+        });
     },
 
     getNodeInfo : function (file) {
@@ -187,7 +201,7 @@ var DetailedUploadedFileTreeDialog = Backbone.View.extend({
 
         title += " <span style='margin-left:20px;'>"+size+"</span>";
 
-        //title += " <span style='margin-left:20px;'><a class='deleteUploadedFile btn btn-danger' data-id='"+file.id+"' data-aiid='"+file.image+"' style='color:white !important;'><i class='glyphicon glyphicon-trash'></i> Delete</a></span>";
+        title += " <span style='margin-left:20px;'><a class='deleteUploadedFile btn btn-danger' data-id='"+file.id+"' data-aiid='"+file.image+"' style='color:white !important;'><i class='glyphicon glyphicon-trash'></i> Delete</a></span>";
 
         if(model.getStatus() === "DEPLOYED"){
             title += " <span style='margin-left:20px;'><a href='#' class='previewUploadedFile' data-url='"+file.thumbURL+"' style='color:#428bca !important;'>See preview</a></span>";
