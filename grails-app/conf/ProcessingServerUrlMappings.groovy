0db871f7fea6066f133734959edf837008323c15
Ulysse Rubens <ulyrubens@hotmail.com>
2018-06-12 17:18:29 +0200

    Add missing update actions in new software management

--- grails-app/conf/ProcessingServerUrlMappings.groovy
+++ grails-app/conf/ProcessingServerUrlMappings.groovy
@@ -1,13 +1,28 @@
+/*
+* Copyright (c) 2009-2017. Authors: see NOTICE file.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 class ProcessingServerUrlMappings {
 
     static mappings = {
         "/api/processing_server.$format"(controller: "restProcessingServer") {
-            action = [GET:"list", POST:"add"]
+            action = [GET: "list", POST: "add"]
         }
 
         "/api/processing_server/$id.$format"(controller: "restProcessingServer") {
-            action = [GET:"show", DELETE:"delete"]
+            action = [GET: "show", PUT: "update", DELETE: "delete"]
         }
     }
 
--- grails-app/conf/SoftwareParameterConstraintUrlMappings.groovy
+++ grails-app/conf/SoftwareParameterConstraintUrlMappings.groovy
@@ -1,13 +1,29 @@
-class SoftwareParameterConstraintUrlMappings{
+/*
+* Copyright (c) 2009-2017. Authors: see NOTICE file.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+class SoftwareParameterConstraintUrlMappings {
 
     static mappings = {
-        "/api/software_parameter_constraint.$json" (controller: "restSoftwareParameterConstraint") {
+        "/api/software_parameter_constraint.$format"(controller: "restSoftwareParameterConstraint") {
             action = [GET: "list", POST: "add"]
         }
-        "/api/software_parameter_constraint/$id.$json" (controller: "restSoftwareParameterConstraint") {
-            action = [GET: "show", DELETE: "delete"]
+        "/api/software_parameter_constraint/$id.$format"(controller: "restSoftwareParameterConstraint") {
+            action = [GET: "show", PUT: "update", DELETE: "delete"]
         }
-        "/api/software_parameter_constraint/evaluate/$id/value/$value.$json" (controller: "restSoftwareParameterConstraint") {
+        "/api/software_parameter_constraint/evaluate/$id/value/$value.$format"(controller: "restSoftwareParameterConstraint") {
             action = [GET: "evaluate", POST: "evaluate"]
         }
     }
