yasushinj/Cytomine-core

commit 4aa75a04e064edc91f11a5a5dcadd20a3e2780f8

Fix 27e2faee2efff2ae4d4f9961d9bcbb3f5c535517 (prevent duplicate rows for image/user couple) + remove join on abstract image in user_image view

grails-app/services/be/cytomine/image/ImageInstanceService.groovy
@@ -114,20 +114,17 @@ class ImageInstanceService extends ModelService {
 
         boolean isAdmin = securityACLService.isAdminByNow(user)
 
-        //user_image already filter nested image
+        // user_image already filter nested image
         def sql = new Sql(dataSource)
-         sql.eachRow("select * from user_image where user_image_id = ? order by original_filename",[user.id]) {
-
-             if(it.project_blind) {
-                 def line = [id:it.id, blindedName:it.base_image_id, projectName:it.project_name,  project:it.project_id]
-                 if(isAdmin || it.user_project_manager) {
-                     line.filename = it.instance_filename;
-                     line.originalFilename = it.original_filename;
-                 }
-                 data << line
-             } else {
-                 data << [id:it.id, filename:it.instance_filename, originalFilename: it.original_filename, projectName:it.project_name,  project:it.project_id]
-             }
+        sql.eachRow("select * from user_image where user_image_id = ? order by instance_filename", [user.id]) {
+            def line = [id: it.id, projectName: it.project_name, project: it.project_id]
+            if(it.project_blind) {
+                line.blindedName = it.base_image_id
+            }
+            if(!it.project_blind || isAdmin || it.user_project_manager) {
+                line.instanceFilename = it.instance_filename
+            }
+            data << line
         }
         sql.close()
         return data

grails-app/services/be/cytomine/utils/database/TableService.groovy
@@ -90,20 +90,22 @@ class TableService {
                                 "AND sec_user.user_id is null AND project.deleted IS NULL"
             createRequest('creator_project',reqcreate)
 
-            reqcreate = "CREATE VIEW user_image AS\n" +
-                    "SELECT distinct image_instance.*, abstract_image.filename, abstract_image.original_filename, project.name as project_name, project.blind_mode as project_blind, sec_user.id as user_image_id, case when mask = 16 then true else false end as user_project_manager\n" +
-                    "FROM project,  image_instance, abstract_image, acl_object_identity, sec_user, acl_sid, acl_entry \n" +
-                    "WHERE project.id = acl_object_identity.object_id_identity\n" +
-                    "AND image_instance.deleted IS NULL \n" +
-                    "AND project.deleted IS NULL \n" +
-                    "AND image_instance.project_id = project.id \n" +
-                    "AND image_instance.parent_id IS NULL \n" + //don't get nested images
-                    "AND abstract_image.id = image_instance.base_image_id\n" +
-                    "AND acl_sid.sid = sec_user.username\n" +
-                    "AND acl_entry.sid = acl_sid.id\n" +
-                    "AND acl_entry.acl_object_identity = acl_object_identity.id\n" +
-                    "AND sec_user.user_id is null\n" +
-                    "AND mask >= 1"
+            reqcreate = "CREATE VIEW user_image AS " +
+                    "SELECT image_instance.*, " +
+                        "project.name as project_name, " +
+                        "project.blind_mode as project_blind, " +
+                        "sec_user.id as user_image_id, " +
+                        "case when MAX(mask) = 16 then true else false end as user_project_manager " +
+                    "FROM image_instance " +
+                    "INNER JOIN sec_user ON sec_user.user_id IS NULL " +
+                    "INNER JOIN project ON project.id = image_instance.project_id AND project.deleted IS NULL " +
+                    "INNER JOIN acl_object_identity ON project.id = acl_object_identity.object_id_identity " +
+                    "INNER JOIN acl_sid ON acl_sid.sid = sec_user.username " +
+                    "INNER JOIN acl_entry ON acl_entry.sid = acl_sid.id " +
+                        "AND acl_entry.acl_object_identity = acl_object_identity.id AND acl_entry.mask >= 1 " +
+                    "WHERE image_instance.deleted IS NULL " +
+                        "AND image_instance.parent_id IS NULL " + // don't get nested images
+                    "GROUP BY image_instance.id, project.id, sec_user.id"
             createRequest('user_image',reqcreate)
 
         } catch (org.postgresql.util.PSQLException e) {

commit b9df9d1063a633b22167834c8cfa46cd1b08fb4c

Fix regression introduced by 4aa75a04e064edc91f11a5a5dcadd20a3e2780f8 (abstract image join in user_image view)

grails-app/services/be/cytomine/image/ImageInstanceService.groovy
@@ -113,7 +113,7 @@ class ImageInstanceService extends ModelService {
                 line.blindedName = it.base_image_id
             }
             if (!it.project_blind || isAdmin || it.user_project_manager) {
-                line.instanceFilename = it.instance_filename
+                line.instanceFilename = it.instance_filename ?: it.original_filename
             }
             data << line
         }

grails-app/services/be/cytomine/utils/database/TableService.groovy
@@ -92,6 +92,7 @@ class TableService {
 
             reqcreate = "CREATE VIEW user_image AS " +
                     "SELECT image_instance.*, " +
+                        "abstract_image.original_filename as original_filename, " +
                         "project.name as project_name, " +
                         "project.blind_mode as project_blind, " +
                         "sec_user.id as user_image_id, " +
@@ -99,13 +100,14 @@ class TableService {
                     "FROM image_instance " +
                     "INNER JOIN sec_user ON sec_user.user_id IS NULL " +
                     "INNER JOIN project ON project.id = image_instance.project_id AND project.deleted IS NULL " +
+                    "INNER JOIN abstract_image ON abstract_image.id = image_instance.base_image_id " +
                     "INNER JOIN acl_object_identity ON project.id = acl_object_identity.object_id_identity " +
                     "INNER JOIN acl_sid ON acl_sid.sid = sec_user.username " +
                     "INNER JOIN acl_entry ON acl_entry.sid = acl_sid.id " +
                         "AND acl_entry.acl_object_identity = acl_object_identity.id AND acl_entry.mask >= 1 " +
                     "WHERE image_instance.deleted IS NULL " +
                         "AND image_instance.parent_id IS NULL " + // don't get nested images
-                    "GROUP BY image_instance.id, project.id, sec_user.id"
+                    "GROUP BY image_instance.id, project.id, sec_user.id, abstract_image.id"
             createRequest('user_image',reqcreate)
 
         } catch (org.postgresql.util.PSQLException e) {