e0c9494ae85da66b17f14a8a2c460ca213f843f2
TheGeekTortoise <Renaud.Hoyoux@yahoo.fr>
2019-06-25 14:36:10 +0200

    pagination userpostions

--- grails-app/controllers/be/cytomine/api/social/RestUserPositionController.groovy
+++ grails-app/controllers/be/cytomine/api/social/RestUserPositionController.groovy
@@ -97,7 +97,11 @@ class RestUserPositionController extends RestController {
         Long afterThan = params.long("afterThan")
         Long beforeThan = params.long("beforeThan")
         if(params.getBoolean("showDetails")){
-            responseSuccess(userPositionService.list(image, user, afterThan, beforeThan))
+            Long max = params.long('max')
+            Long offset = params.long('offset')
+            params.max = 0
+            params.offset = 0
+            responseSuccess(userPositionService.list(image, user, afterThan, beforeThan, max, offset))
         } else {
             responseSuccess(userPositionService.summarize(image, user, afterThan, beforeThan))
         }
