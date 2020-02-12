f0fa2803011a06248c0057b5eabd94e274ba31a7
Ulysse Rubens <urubens@uliege.be>
2020-02-12 09:47:39 +0100

    Fix previous cherry-pick

--- grails-app/jobs/be/cytomine/job/ExtractImageMetadataJob.groovy
+++ grails-app/jobs/be/cytomine/job/ExtractImageMetadataJob.groovy
@@ -17,19 +17,35 @@ package be.cytomine.job
 */
 
 import be.cytomine.image.AbstractImage
+import be.cytomine.image.UploadedFile
 
 class ExtractImageMetadataJob {
 
     def imagePropertiesService
+    def sampleHistogramService
 
     static triggers = {
-        //simple name: 'extractImageMetadataJob', startDelay: 60000, repeatInterval: 1000*60
+        simple name: 'extractImageMetadataJob', startDelay: 10000, repeatInterval: 1000*20
     }
 
     def execute() {
-        Collection<AbstractImage> abstractImages = AbstractImage.findAllByWidthIsNullOrWidthLike(-1)
+        Collection<AbstractImage> abstractImages = AbstractImage.findAllBySamplePerPixelIsNullOrWidthIsNullOrWidth(-1, [max: 10, sort: "created", order: "desc"])
         abstractImages.each { image ->
-            imagePropertiesService.extractUseful(image)
+            try {
+                UploadedFile.withNewSession {
+                    AbstractImage.withNewSession {
+                        image.attach()
+                        log.info "Regenerate properties for image $image - ${image.originalFilename}"
+                        imagePropertiesService.regenerate(image)
+                        if (image.bitPerSample > 8)
+                            sampleHistogramService.extractHistogram(image)
+                    }
+                }
+            }
+            catch (Exception e) {
+                log.error "Error during metadata extraction for image $image: ${e.printStackTrace()}"
+            }
+
         }
     }
 }
