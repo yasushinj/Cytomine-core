adc3b1a0cacf4ab65a00db976a5a12c774369409
TheGeekTortoise <Renaud.Hoyoux@yahoo.fr>
2020-08-27 13:34:42 +0200

    add comment

--- src/java/be/cytomine/annotations/DependencyOrder.java
+++ src/java/be/cytomine/annotations/DependencyOrder.java
@@ -7,6 +7,12 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
+/**
+ * Determines a priority into dependencies.
+ * */
 public @interface DependencyOrder {
+    /**
+     * A method with an higher value will be executed before the others.
+     * */
     public int order() default Integer.MIN_VALUE;
 }
